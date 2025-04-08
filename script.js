d1dfaab45f057b641107e10390f239bba6b051b6
gnzl00 <9200492@alumnos.ufv.es>
2025-04-08 09:23:38 +0200

    Comit inicial
    Planteamos la estructura basica del proyeccto con un index, un css y un JS. Faltaria por agregar un logo

---
+++ script.js
@@ -0,0 +1,8 @@
+function descargarApp() {
+    alert("Próximamente disponible en App Store y Google Play. ¡Mantente atento!");
+}
+
+document.getElementById("formContacto").addEventListener("submit", function(e) {
+    e.preventDefault();
+    alert("Gracias por tu mensaje. Te contactaremos pronto.");
+});
